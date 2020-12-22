d2db49512308061189b1ea113522005e7134729c
pndnism <p.dai.man8@gmail.com>
2020-12-23 01:36:49 +0900

    [add] deviseを使って認証機能を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = ["yamada", "abe", "tanaka", "yave", "kitani"]
+users.each_with_index do |user, i|
+  User.create(
+    email: "#{i + 1}@gamil.com",
+    encrypted_password: "password"
+  )
+end
