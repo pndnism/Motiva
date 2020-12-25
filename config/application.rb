4c3d7214fc8b1bed678df08b4eb476cc164ebf41
pndnism <p.dai.man8@gmail.com>
2020-12-25 19:47:33 +0900

    [fix] before_actionが悪さをしていたのを修正

--- config/application.rb
+++ config/application.rb
@@ -10,15 +10,6 @@ module Motiva
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    before_action :authenticate_user!
-  
-    def after_sign_in_path_for(resource)
-      "http://www.google.co.jp/" # ログイン後に遷移するpathを設定
-    end
-  
-    def after_sign_out_path_for(resource)
-      "http://www.google.co.jp/" # ログアウト後に遷移するpathを設定
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
