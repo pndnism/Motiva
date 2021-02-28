e51e5acd1a6959f53153a6ca9736e2f8ebdd095d
pndnism <p.dai.man8@gmail.com>
2021-02-28 18:20:23 +0900

    [add] seedファイルの定義を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "pman", mail: "p.dai.man8@gmail.com", password:"password")
+User.create(name: "kameoka", mail: "kamekame@gmail.com", password:"password")
+User.create(name: "tateno", mail: "ponpon8@gmail.com", password:"password")
+
+Habit.create(user_id: 1, personal_habit_id: 1, habit:"ストレッチ", habituation:0)
+Habit.create(user_id: 1, personal_habit_id: 2, habit:"自炊", habituation:0)
+Habit.create(user_id: 2, personal_habit_id: 1, habit:"床掃除", habituation:0)
